18f7f74f88fa8502163651056e99442bb15bee2a
J-Turner-Dev <jturner.dev@gmail.com>
2022-04-24 11:41:06 -0500

    Note making complete

--- frontend/src/pages/NotePage.js
+++ frontend/src/pages/NotePage.js
@@ -1,32 +1,87 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const NotePage = () => {
   let params = useParams();
   let [note, setNote] = useState(null);
+  let navigate = useNavigate();
 
   useEffect(() => {
     getNote();
   }, [params.id]);
 
   let getNote = async () => {
+    if (params.id === "new") return;
     let response = await fetch(`/api/notes/${params.id}/`);
     let data = await response.json();
     setNote(data);
+  };
+
+  let createNote = async () => {
+    fetch(`/api/notes/create/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(note),
+    });
+  };
+
+  let updateNote = async () => {
+    fetch(`/api/notes/${params.id}/update/`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(note),
+    });
+  };
+
+  let deleteNote = async () => {
+    fetch(`/api/notes/${params.id}/delete/`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    navigate("/");
+  };
+
+  let handleSubmit = () => {
+    if (params.id !== "new" && note.body === "") {
+      deleteNote();
+    } else if (params.id !== "new") {
+      updateNote();
+    } else if (params.id === "new" && note !== null) {
+      createNote();
+    }
+    navigate("/");
+  };
+
+  let handleChange = (value) => {
+    setNote((note) => ({ ...note, body: value }));
   };
 
   return (
     <div className="note">
       <div className="note-header">
         <h3>
-          <Link to="/">
-            <ArrowLeft />
-          </Link>
+          <ArrowLeft onClick={handleSubmit} />
         </h3>
+        {params.id !== "new" ? (
+          <button onClick={deleteNote}>Delete</button>
+        ) : (
+          <button onClick={handleSubmit}>Done</button>
+        )}
       </div>
-      <textarea defaultValue={note?.body}></textarea>
+      <textarea
+        onChange={(e) => {
+          handleChange(e.target.value);
+        }}
+        value={note?.body}
+      ></textarea>
     </div>
   );
 };
